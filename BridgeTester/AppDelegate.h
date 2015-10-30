79cd9a645f4a0a5c77fa1dd7f77e64fe415adfc2
LiuXiangyu <liuxiangyu@live.com>
2015-10-30 14:06:24 +0800

    Initial Commit

---
+++ BridgeTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgeTester
+//
+//  Created by LiuXiangyu on 10/30/15.
+//  Copyright © 2015 LiuXiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BridgeTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgeTester
+//
+//  Created by LiuXiangyu on 10/30/15.
+//  Copyright © 2015 LiuXiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
